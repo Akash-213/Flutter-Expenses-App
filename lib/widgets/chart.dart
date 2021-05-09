73c03e163c22c6c6e2ba100c421db0c5a3b76d0f
Akash Kulkarni <67867515+Akash-213@users.noreply.github.com>
2021-05-09 17:53:46 +0530

    Update chart.dart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -38,7 +38,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
+    //print(groupedTransactionValues);
     return Card(
       elevation: 5,
       margin: EdgeInsets.all(15),
